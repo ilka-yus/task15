8b14a2da6b6aa29febe0bf969473983a8219495e
ilka-yus <ilnara.u06@gmail.com>
2025-07-16 21:00:30 +0500

    task15

--- app/models.py
+++ app/models.py
@@ -4,7 +4,6 @@ from datetime import datetime
 
 class User(SQLModel, table=True):
     __tablename__ = "task15_user"
-    __table_args__ = {"schema": "public"}
 
     id: Optional[int] = Field(default=None, primary_key=True)
     username: str = Field(index=True, unique=True)
@@ -14,10 +13,9 @@ class User(SQLModel, table=True):
 
 class Note(SQLModel, table=True):
     __tablename__ = "task15_note"
-    __table_args__ = {"schema": "public"}
 
     id: Optional[int] = Field(default=None, primary_key=True)
     text: str
     created_at: datetime = Field(default_factory=datetime.utcnow)
-    owner_id: int = Field(foreign_key="public.task15_user.id")
+    owner_id: int = Field(foreign_key="task15_user.id")
     owner: Optional[User] = Relationship(back_populates="notes")
